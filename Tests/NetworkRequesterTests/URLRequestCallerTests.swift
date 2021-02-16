e9241e887787342bea2f2f67a10b3363c04a1d76
Ivaylo Gashev <ivaylo.gashev@gmail.com>
2021-02-16 21:54:52 +0200

    added more caller tests

--- Tests/NetworkRequesterTests/URLRequestCallerTests.swift
+++ Tests/NetworkRequesterTests/URLRequestCallerTests.swift
@@ -14,8 +14,9 @@ final class URLRequestCallerTests: XCTestCase {
     
     func testCallSucceeds() {
         let caller = URLRequestCaller(
-            decoder: decoder, getDataPublisher: { [encodedData] _ in
-                Just((encodedData, .success))
+            decoder: decoder,
+            getDataPublisher: { [encodedData] _ in
+                Just((encodedData, .withStatus(200)))
                     .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
                     .eraseToAnyPublisher()
             }
@@ -39,7 +40,7 @@ final class URLRequestCallerTests: XCTestCase {
         .store(in: &cancellables)
     }
     
-    func testCallFails() {
+    func testCallFailsDueToURLError() {
         let caller = URLRequestCaller(
             decoder: decoder,
             getDataPublisher: { _ in Fail(error: URLError(.badURL)).eraseToAnyPublisher() }
@@ -65,12 +66,85 @@ final class URLRequestCallerTests: XCTestCase {
             .store(in: &cancellables)
     }
     
+    func testCallFailsDueToInvalidHTTPResponse() {
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((.init(), .withStatus(1000)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
+        
+        caller.call(request: URLRequest(url: URL(string: "https://google.com")!))
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        switch error {
+                        case .networking(let status):
+                            switch status {
+                            case .internalServerError:
+                                break
+                            default:
+                                XCTFail("A wrong error is thrown. Expected NetworkingError.networking(status: .internalServerError), got \(error).")
+                            }
+                        default:
+                            XCTFail("A wrong error is thrown. Expected NetworkingError.unknown, got \(error).")
+                        }
+                    case .finished:
+                        XCTFail("Expected NetworkingError.unknown, got .finished completion.")
+                    }
+                },
+                receiveValue: { data in XCTFail("Expected NetworkingError.unknown, got \(data).") }
+            )
+            .store(in: &cancellables)
+    }
+    
+    func testCallFailsDueToBadRequest() {
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((.init(), .withStatus(HTTPStatus.badRequest.code)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
+        
+        caller.call(request: URLRequest(url: URL(string: "https://google.com")!))
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        switch error {
+                        case .networking(let status):
+                            switch status {
+                            case .badRequest:
+                                break
+                            default:
+                                XCTFail("A wrong error is thrown. Expected NetworkingError.networking(status: .badRequest), got \(error).")
+                            }
+                        default:
+                            XCTFail("A wrong error is thrown. Expected NetworkingError.unknown, got \(error).")
+                        }
+                    case .finished:
+                        XCTFail("Expected NetworkingError.unknown, got .finished completion.")
+                    }
+                },
+                receiveValue: { data in XCTFail("Expected NetworkingError.unknown, got \(data).") }
+            )
+            .store(in: &cancellables)
+    }
+    
     func testResponseToBeIntButGotEmpty() {
-        let caller = URLRequestCaller(decoder: decoder) { _ in
-            Just((data: .init(), response: .success))
-                .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
-                .eraseToAnyPublisher()
-        }
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((data: .init(), response: .withStatus(200)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
         
         let intResponsePublisher: AnyPublisher<Int, NetworkingError> = caller.call(request: request)
         intResponsePublisher
@@ -94,11 +168,14 @@ final class URLRequestCallerTests: XCTestCase {
     }
     
     func testResponseToBeEmptyButGotData() {
-        let caller = URLRequestCaller(decoder: decoder) { [encodedData] _ in
-            Just((data: encodedData, response: .success))
-                .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
-                .eraseToAnyPublisher()
-        }
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { [encodedData] _ in
+                Just((data: encodedData, response: .withStatus(200)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
         
         caller.call(request: request)
             .sink(
@@ -119,6 +196,120 @@ final class URLRequestCallerTests: XCTestCase {
             )
             .store(in: &cancellables)
     }
+    
+    func testResponseToBeEmptyAndGotEmpty() {
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((data: .init(), response: .withStatus(200)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
+        
+        caller.call(request: request)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        XCTFail("Expected to receive value. Got error: \(error)")
+                    case .finished:
+                        break
+                    }
+                },
+                receiveValue: { _ in }
+            )
+            .store(in: &cancellables)
+    }
+    
+    func testResponseToBeEmptyAndGotEmptyUsingURLRequestBuilder() {
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((data: .init(), response: .withStatus(200)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
+        
+        let builder = URLRequestBuilder(environment: Environment(), endpoint: Environment(), httpMethod: .get)
+        caller.call(builder: builder)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        XCTFail("Expected to receive value. Got error: \(error)")
+                    case .finished:
+                        break
+                    }
+                },
+                receiveValue: { _ in }
+            )
+            .store(in: &cancellables)
+    }
+    
+    func testResponseToBeEmptyToFailDueToBuilderError() {
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((data: .init(), response: .withStatus(200)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
+        
+        let builder = URLRequestBuilder(environment: Environment(url: "dad asdas"), endpoint: Environment(url: "dad asdas"), httpMethod: .get)
+        caller.call(builder: builder)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        switch error {
+                        case .buildingURL:
+                            break
+                        default:
+                            XCTFail("A wrong error is thrown. Expected NetworkingError.buildingURL, got \(error).")
+                        }
+                    case .finished:
+                        XCTFail("Expected NetworkingError.buildingURL, got .finished completion.")
+                    }
+                },
+                receiveValue: { data in XCTFail("Expected NetworkingError.buildingURL, got \(data).") }
+            )
+            .store(in: &cancellables)
+    }
+    
+    func testResponseDataToFailDueToBuilderError() {
+        let caller = URLRequestCaller(
+            decoder: decoder,
+            getDataPublisher: { _ in
+                Just((data: .init(), response: .withStatus(200)))
+                    .setFailureType(to: URLRequestCaller.AnyURLSessionDataPublisher.Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        )
+
+        let builder = URLRequestBuilder(environment: Environment(url: "dad asdas"), endpoint: Environment(url: "dad asdas"), httpMethod: .get)
+        let intResponsePublisher: AnyPublisher<Int, NetworkingError> = caller.call(builder: builder)
+        intResponsePublisher
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        switch error {
+                        case .buildingURL:
+                            break
+                        default:
+                            XCTFail("A wrong error is thrown. Expected NetworkingError.buildingURL, got \(error).")
+                        }
+                    case .finished:
+                        XCTFail("Expected NetworkingError.buildingURL, got .finished completion.")
+                    }
+                },
+                receiveValue: { data in XCTFail("Expected NetworkingError.buildingURL, got \(data).") }
+            )
+            .store(in: &cancellables)
+    }
 }
 
 private extension URLRequestCallerTests {
@@ -126,13 +317,17 @@ private extension URLRequestCallerTests {
         let name: String
         let age: Int
     }
+    
+    struct Environment: URLProviding {
+        var url: String = "https://google.com"
+    }
 }
 
 private extension URLResponse {
-    static var success: HTTPURLResponse {
+    static func withStatus(_ status: Int) -> HTTPURLResponse {
         HTTPURLResponse(
             url: URL(string: "https://google.com")!,
-            statusCode: 200,
+            statusCode: status,
             httpVersion: nil,
             headerFields: nil
         )!
